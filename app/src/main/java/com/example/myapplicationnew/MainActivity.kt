28be983707e8bfd5a3bcb3a2d55cd7289676b99f
t_murata13 <murata.taijiro2015@gmail.com>
2021-01-18 21:33:24 +0900

    じゃんけんアプリ完成

--- app/src/main/java/com/example/myapplicationnew/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationnew/MainActivity.kt
@@ -1,11 +1,38 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.myapplicationnew
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.text.method.TextKeyListener.clear
+import android.view.View
+import androidx.core.content.edit
+import com.example.myapplicationnew.R.layout.activity_main
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+
+        gu.setOnClickListener { onJankenButtonTapped(it) }
+        choki.setOnClickListener { onJankenButtonTapped(it) }
+        pa.setOnClickListener { onJankenButtonTapped(it) }
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.edit {
+            clear()
+        }
+
+    }
+
+
+
+    fun onJankenButtonTapped(view: View?) {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("MY_HAND", view?.id)
+        startActivity(intent)
     }
 }
